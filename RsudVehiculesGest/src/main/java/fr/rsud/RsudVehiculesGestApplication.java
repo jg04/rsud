ccebe2c33d50e10fea7ed55e4d9e9e35412e47a6
jose <jgarcia11@orange.fr>
2018-06-30 18:15:07 +0200

    Deuxième commit

---
+++ RsudVehiculesGest/src/main/java/fr/rsud/RsudVehiculesGestApplication.java
@@ -0,0 +1,18 @@
+package fr.rsud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsudVehiculesGestApplication {
+
+	
+	//
+	public static void main(String[] args) {
+		SpringApplication.run(RsudVehiculesGestApplication.class, args);
+	}
+	
+	private void att() {
+		
+	}
+}
